9af9a66ba822be921d908ff754619ed5d44df082
inka.179 <ewelinadalek@gmail.com>
2021-06-26 19:01:14 +0200

    creating list of diagonal to iteration

--- move_0.py
+++ move_0.py
@@ -1,26 +1,39 @@
-col=5
-row=5
+import random
+def diagonal1(col, row):
+    lst1=[]
+    lst1.append([a for a in range(col-1,-1,-1)])
+    lst1.append([b for b in range(0, row)])
+    lst=[]
+    for i in zip(lst1[0], lst1[1]):
+        k=list(i)
+        lst.append(k)
+    return lst
+
+def diagonal2(col, row):
+    lst2=[]
+    lst2.append([a for a in range(col-1, -1, -1)])
+    lst2.append([b for b in range(row-1, -1, -1)])
+    lstt=[]
+    for i in zip(lst2[0],lst2[1]):
+        k=list(i)
+        lstt.append(k)
+    return lstt
+
+x=diagonal1(2,2)
+y=diagonal2(2,2)
+diagonals=x+y
+#print(diagonals)
+#losowe współrzędne:
+col=6
+row=6
+def rand_coord(x,y):
+    rand_row=random.randint(0, x-1)
+    rand_col=random.randint(0, y-1)
+    lst=[rand_row, rand_col]
+    return lst
+print(rand_coord(col, row))
+mylist=iter([x for x in diagonals])
+x=next(mylist, rand_coord(col, row))
+print(x)
 
 
-#def creating move 0
-lst1=[]
-lst1.append([a for a in range(col-1,-1,-1)])
-lst1.append([b for b in range(0, row)])
-print(lst1)
-lst=[]
-for i in zip(lst1[0], lst1[1]):
-    k=list(i)
-    lst.append(k)
-print(lst)
-
-lst2=[]
-lst2.append([a for a in range(col-1, -1, -1)])
-lst2.append([b for b in range(row-1, -1, -1)])
-lstt=[]
-for i in zip(lst2[0],lst2[1]):
-    k=list(i)
-    lstt.append(k)
-print(lstt)
-#for el in LAST_HIT:
-#board.change_to_zatopiony(el[0],el[1])
-#
