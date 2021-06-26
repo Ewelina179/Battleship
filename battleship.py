db9f2ff1fe43472d1466288acd7fcff95a6b7156
inka.179 <ewelinadalek@gmail.com>
2021-06-26 10:26:36 +0200

    connection move 0 with func when ship is found

--- battleship.py
+++ battleship.py
@@ -69,34 +69,32 @@ board=ShipBoard(board_of)
 #print(board)
 #board.show_board(board_of)
 
-ship1=Ship(4,4,"unfully",board, 3)
-
-while ship1.form=="unfully":
-    q,w=ship1.row, ship1.col
-    board.change_to_trafiony(q,w)
-    print(board)
-    board.show_board(board_of)
-    global LAST_HIT
-    LAST_HIT=[]
-    LAST_HIT.append([q,w])
-    #LAST_HIT.append([2,2])
-    
-    
-
-    
-    LAST_HIT=[[q,w]]    
-    for el in LAST_HIT: 
-        for method in range(0,4):
-            print(LAST_HIT)
-            lst_of_methods=[ship1.first_move(el[0], el[1]), ship1.second_move(el[0], el[1]), ship1.third_move(el[0], el[1]), ship1.fourth_move(el[0], el[1])]
+        #if kolejny jest empty to go podaj!
+        #kolejna propozycja ruchu z listy. odczyt z pliku, zakodować i wysłać. o ile już nie padł ruch!!!! uwzględnij to!
+        
+def find_rest_of_ship(ship1):
+    ship1.form=="unfully"
+    while ship1.form=="unfully":
+        q,w=ship1.row, ship1.col    
+        board.change_to_trafiony(q,w)
+        print(board)
+        board.show_board(board_of)
+        global LAST_HIT
+        LAST_HIT=[]
+        LAST_HIT.append([q,w])
+        LAST_HIT=[[q,w]]    
+        for el in LAST_HIT: 
+            for method in range(0,4):
+                print(LAST_HIT)
+                lst_of_methods=[ship1.first_move(el[0], el[1]), ship1.second_move(el[0], el[1]), ship1.third_move(el[0], el[1]), ship1.fourth_move(el[0], el[1])]
             
-            x=int(lst_of_methods[method][0]) 
-            y=int(lst_of_methods[method][1])
+                x=int(lst_of_methods[method][0]) 
+                y=int(lst_of_methods[method][1])
             
-            if board.is_empty(x,y)==False:
+                if board.is_empty(x,y)==False:
                     print("unavailable move")    
                     continue
-            elif board.is_empty(x,y):
+                elif board.is_empty(x,y):
                     print(x,y)
                     z=input("Podaj stan współrzędnych: ")
                     
@@ -109,7 +107,8 @@ while ship1.form=="unfully":
                         for el in LAST_HIT:
                             board.change_to_zatopiony(el[0],el[1])
                         board.show_board(board_of)
-                        ship1.form="fully"
+                        #ship1.form="fully"
+                        ship1.form!="unfully"
                         break
                     #nie przerywa głównej pętli while. do poprawki
                     if z=="trafiony":
@@ -117,4 +116,16 @@ while ship1.form=="unfully":
                         print(board)
                         board.show_board(board_of)
                         LAST_HIT.append([x,y])
-                        
+
+#przychodzące - raise Exception na wszelki wypadek?    
+coord=[4,4]#przykładowe "kolejne współrzędne" z mojej listy proponowanych ruchów, która jeszcze nie powstała ;p
+print(coord)                
+c=input("Podaj stan współrzędnych: ")#informacja otrzymana - funkcje - przyjmij, przerób na swoje
+if c=="trafiony":
+    ship1=Ship(coord[0],coord[1],"unfully",board, 3)
+    find_rest_of_ship(ship1)
+        #tu powinna być funckja do tego przebiegu
+elif c=="nietrafiony":
+    a==b
+     #if kolejny jest empty to go podaj!
+        #kolejna propozycja ruchu z listy. odczyt z pliku, zakodować i wysłać. o ile już nie padł ruch!!!! uwzględnij to!
