55046ce6f4e6fc4476334d5d6b726947bbc00df6
inka.179 <ewelinadalek@gmail.com>
2021-07-03 16:43:01 +0200

    removing unnecessary code

--- move_0.py
+++ move_0.py
@@ -1,4 +1,5 @@
 import random
+
 def diagonal1(col, row):
     lst1=[]
     lst1.append([a for a in range(col-1,-1,-1)])
@@ -10,30 +11,20 @@ def diagonal1(col, row):
     return lst
 
 def diagonal2(col, row):
-    lst2=[]
-    lst2.append([a for a in range(col-1, -1, -1)])
-    lst2.append([b for b in range(row-1, -1, -1)])
-    lstt=[]
-    for i in zip(lst2[0],lst2[1]):
+    lst1=[]
+    lst1.append([a for a in range(col-1, -1, -1)])
+    lst1.append([b for b in range(row-1, -1, -1)])
+    lst=[]
+    for i in zip(lst1[0],lst1[1]):
         k=list(i)
-        lstt.append(k)
-    return lstt
+        lst.append(k)
+    return lst
 
-x=diagonal1(2,2)
-y=diagonal2(2,2)
-diagonals=x+y
-#print(diagonals)
-#losowe współrzędne:
-col=6
-row=6
 def rand_coord(x,y):
     rand_row=random.randint(0, x-1)
     rand_col=random.randint(0, y-1)
     lst=[rand_row, rand_col]
     return lst
-print(rand_coord(col, row))
-mylist=iter([x for x in diagonals])
-x=next(mylist, rand_coord(col, row))
-print(x)
+
 
 
